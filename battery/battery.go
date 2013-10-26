c156127f0de6dc2401bd68c4678422cacffb3762
Ian Denhardt <ian@zenhack.net>
2013-10-26 01:01:29 -0400

    Massive refactoring
    Moved all of the battery stuff into its own module.

---
+++ battery/battery.go
@@ -0,0 +1,114 @@
+package battery
+
+import (
+	"io/ioutil"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+type State struct {
+	// true iff the battery is being charged.
+	Charging bool
+	// The percentage of the battery's maximum energy remaining.
+	PercentRemaining int
+	// Time until battery is exhausted. Only meaningful if Charging is false.
+	Hours, Minutes, Seconds int
+}
+
+type Battery struct {
+	Path string
+	State *State
+}
+
+func (s *State) String() string {
+	if s.Charging {
+		return fmt.Sprintf("Battery, charging, %d%%", s.PercentRemaining)
+	} else {
+		return fmt.Sprintf("On battery, %d:%02d:%02d remaining (%d%%)",
+			s.Hours, s.Minutes, s.Seconds, s.PercentRemaining)
+	}
+}
+
+func (b *Battery) Poll() {
+	var err error
+
+	// Read in each of the files we care about, and store their contents in a map.
+	text := make(map[string] string)
+	for _, f := range []string{"status", "capacity", "power_now", "energy_now" } {
+		text[f], err = slurpFile(b.Path + f)
+		if err != nil {
+			// If we're unsuccessful reading any of the files, stop now.
+			// We don't want to update the state until we can get a
+			// meaningful reading.
+			return
+		}
+	}
+
+	// Most of the files contain numbers; convert them into actual
+	// integers so we can work with them.
+	nums := make(map[string] int)
+	for _, f := range []string{"capacity", "power_now", "energy_now"} {
+		// The call to TrimSpace is important; the files end with a
+		// newline, so without this Atoi will fail.
+		nums[f], err = strconv.Atoi(strings.TrimSpace(text[f]))
+		if err != nil {
+			// Again, stop now if anything fails.
+			return
+		}
+	}
+
+	// assigning these to variables will make the code below less verbose
+	capacity, power, energy := nums["capacity"], nums["power_now"], nums["energy_now"]
+
+	if power == 0 {
+		// If power is zero, it is usually an indication that the
+		// device has been recently plugged in or unplugged, and
+		// hasn't settled down yet. Stop here; we want to wait until
+		// we can get an accurate reading.
+		return
+	}
+
+	result := &State{}
+
+	// work out how much time we have left
+	result.Hours = energy/power
+	energy %= power
+	result.Minutes = (60*energy) / power
+	energy = (60*energy) % power
+	result.Seconds = (60*energy) / power
+
+	result.PercentRemaining = capacity
+
+	result.Charging = (text["status"][0] == 'C')
+	b.State = result
+}
+
+func (b *Battery) Monitor() {
+	if !b.exists() {
+		return
+	}
+	for {
+		b.Poll()
+		time.Sleep(time.Second)
+	}
+}
+
+// This is a utility function which grabs the entire contents of a file by
+// name. We use this in the Poll method.
+func slurpFile(filename string) (string, error) {
+	file, err := os.Open(filename)
+	if err != nil {
+		return "", err
+	}
+	defer file.Close()
+	data, err := ioutil.ReadAll(file)
+	return string(data), err
+}
+
+func (b *Battery) exists() bool {
+	_, err := os.Stat(b.Path)
+	return err == nil
+}
--- main.go
+++ main.go
@@ -1,125 +1,33 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"strconv"
-	"strings"
 	"time"
+
+	"github.com/zenhack/my-status-monitor/battery"
 )
 
 const (
-	timeFormat = "Mon Jan 2 3:04 PM"
-	batteryPath = "/sys/class/power_supply/BAT0/"
 )
-
-var (
-	deviceNotReady = errors.New("Device is not ready for polling")
-)
-
-var (
-	battInfo *BatteryInfo
-)
-
-type BatteryInfo struct {
-	hour, min, sec int // time remaining
-	capacity int // percent charged
-	charging bool
-}
-
-func (b *BatteryInfo) String() string {
-	if b.charging {
-		return fmt.Sprintf("Battery charging, %d%%", b.capacity)
-	} else {
-		return fmt.Sprintf("On battery, %d:%02d:%02d remaining (%d%%)",
-			b.hour, b.min, b.sec, b.capacity)
-	}
-}
 
 func emit(str string) {
 	cmd := exec.Command("xsetroot", "-name", str)
 	cmd.Run()
 }
 
-func haveBattery() bool {
-	_, err := os.Stat(batteryPath)
-	return err == nil
-}
-
-func slurpFile(filename string) (string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-	data, err := ioutil.ReadAll(file)
-	return string(data), err
-}
-
-func pollBattery() (*BatteryInfo, error) {
-	var err error
-	text := make(map[string] string)
-	for _, f := range []string{"status", "capacity", "power_now", "energy_now" } {
-		text[f], err = slurpFile(batteryPath + f)
-		if err != nil {
-			return nil, err
-		}
-		text[f] = strings.TrimSpace(text[f])
-	}
-
-	nums := make(map[string] int)
-	for _, f := range []string{"capacity", "power_now", "energy_now"} {
-		nums[f], err = strconv.Atoi(text[f])
-		if err != nil {
-			fmt.Printf("%#v", text[f])
-			return nil, err
-		}
-	}
-
-	capacity, power, energy := nums["capacity"], nums["power_now"], nums["energy_now"]
-
-	if power == 0 {
-		// We should wait for the device to settle.
-		return nil, deviceNotReady
-	}
-
-	result := &BatteryInfo{}
-
-	// work out how much time we have left
-	result.hour = energy/power
-	energy %= power
-	result.min = (60*energy) / power
-	energy = (60*energy) % power
-	result.sec = (60*energy) / power
-
-	result.capacity = capacity
-
-	result.charging = (text["status"][0] == 'C')
-	return result, nil
-}
 
 func main() {
-	go func() {
-		if !haveBattery() {
-			return
-		}
-		for {
-			info, err := pollBattery()
-			if err == nil {
-				battInfo = info
-			}
-			time.Sleep(time.Second)
-		}
-	}()
+	timeFormat := "Mon Jan 2 3:04 PM"
+	myBattery := &battery.Battery{Path: "/sys/class/power_supply/BAT0/"}
+
+	go myBattery.Monitor()
 
 	for {
 		t := time.Now()
-		batt := battInfo
-		if batt != nil {
-			emit(fmt.Sprintf("%v | %s", batt, t.Format(timeFormat)))
+		state := myBattery.State
+		if state != nil {
+			emit(fmt.Sprintf("%v | %s", state, t.Format(timeFormat)))
 		} else {
 			emit(t.Format(timeFormat))
 		}
