aaeaf3bd707ff9e8ab37b5ccd638f41497d19ee4
Ian Denhardt <ian@zenhack.net>
2014-06-22 00:59:59 -0400

    Some re-working of the retry behavior
    This doesn't actually work because of a DNS issue; I'm going to go back
    to invoking the shell script, but check the response code. That will be
    in the next commit, but I'm keeping the backoff stuff from here.

--- weather/weather.go
+++ weather/weather.go
@@ -1,12 +1,19 @@
 package weather
 
 import (
+	"errors"
 	"io"
 	"net/http"
 	"os"
 	"os/exec"
 	"strings"
 	"time"
+
+	"fmt"
+)
+
+var (
+	httpNotOK = errors.New("HTTP response was not 200 OK")
 )
 
 const (
@@ -19,6 +26,9 @@ func fetch() error {
 	resp, err := http.Get(YWeatherEndpoint + WOEID_BOSTON)
 	if err != nil {
 		return err
+	}
+	if resp.StatusCode != 200 {
+		return httpNotOK
 	}
 	defer resp.Body.Close()
 	file, err := os.Create(os.Getenv("HOME") + "/.cache/weather")
@@ -39,12 +49,26 @@ func show() string {
 func Monitor() <-chan string {
 	ch := make(chan string, 1)
 	fetch()
-	go func() {
-		for {
-			ch <- show()
-			time.Sleep(20 * time.Minute)
-			fetch()
-		}
-	}()
+	go monitor(ch)
 	return ch
+}
+
+func monitor(ch chan<- string) {
+	for {
+		backoff := time.Second
+		err := fetch()
+		for err != nil {
+			fmt.Println(err)
+			backoff *= 2
+			if backoff > time.Minute / 2 {
+				backoff = time.Minute / 2
+			}
+			fmt.Println("backoff = ", backoff)
+			time.Sleep(backoff)
+			err = fetch()
+		}
+		ch <- show()
+		time.Sleep(20 * time.Minute)
+	}
+
 }
