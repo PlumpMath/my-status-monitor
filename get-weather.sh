6208e280e6877df743f8afd9a3e4c5183adfacac
Ian Denhardt <ian@zenhack.net>
2014-06-21 21:09:44 -0400

    The daemon now fetches the weather itself.
    Before it was calling out to get-weather.sh, which has been deleted.

--- get-weather.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-WOEID=2367105 # Boston
-
-curl "http://weather.yahooapis.com/forecastrss?w=${WOEID}" > $HOME/.cache/weather
--- weather/weather.go
+++ weather/weather.go
@@ -1,14 +1,33 @@
 package weather
 
 import (
+	"io"
+	"net/http"
+	"os"
 	"os/exec"
 	"strings"
 	"time"
 )
 
+const (
+	WOEID_BOSTON = "2367105"
+
+	YWeatherEndpoint = "https://weather.yahooapis.com/forecastrss?w="
+)
+
 func fetch() error {
-	cmd := exec.Command("get-weather.sh")
-	return cmd.Run()
+	resp, err := http.Get(YWeatherEndpoint + WOEID_BOSTON)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+	file, err := os.Create(os.Getenv("HOME") + "/.cache/weather")
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+	_, err = io.Copy(file, resp.Body)
+	return err
 }
 
 func show() string {
